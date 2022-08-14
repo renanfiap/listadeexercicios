7a8087242b626f0ef4286cfeea4ea7994dc9e32a
Renan Ramos <renan_dasilvaramos@hotmail.com>
2022-08-14 20:51:21 -0300

    acrescentando os enunciados

--- Program.cs
+++ Program.cs
@@ -1,2 +1,70 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. Fazer um programa que leia três valores com ponto flutuante: A, B e C. Em seguida, calcule e mostre:
+// a) a área do triângulo retângulo que tem A por base e C por altura. Fórmula: área = (base * altura) / 2
+// b) a área do círculo de raio C. Fórmula: área = pi * raio²
+// c) a área do trapézio que tem A e B por bases e C por altura. Fórmula: área = ((base1 + base2) * altura) / 2
+// d) a área do quadrado que tem lado B. Fórmula: área = base²
+// e) a área do retângulo que tem lados A e B. Fórmula: área = base * altura
+
+
+
+
+
+
+
+// 2. Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "São Múltiplos" ou "Não são
+// Multiplos", indicando se os valores lidos são múltiplos entre si.
+
+
+
+// 3. A estimativa de três pontos (PERT) é uma técnica utilizada para calcular a duração de uma atividade baseada em
+// três valores:
+// • A estimativa mais otimista;
+// • A estimativa mais pessimista; e
+// • A estimativa mais provável.
+// Para se calcular o tempo esperado para uma atividade, utiliza-se a seguinte fórmula:
+
+// Crie um programa que leia a estimativa (em horas) otimista, a pessimista e a mais provável e exiba na tela o tempo
+// esperado, conforme a fórmula acima. Ex.:
+// Otimista: 10
+// Pessimista: 30
+// Provavel: 15
+// PERT = 16.66
+
+
+// 4. Escreva um programa que leia a nota de um aluno e imprima sua situação com base nas seguintes condições:
+// a) Caso a nota seja >= 7: “Aprovado”
+// b) Caso a nota seja < 7 e >= 4: “Em recuperação”
+// c) Caso a nota seja < 4: “Reprovado”
+
+// 5. Escreva um programa, utilizando a estrutura de decisão switch/case, que receba do usuário um número inteiro de 1
+// a 3 e imprima a mensagem na tela com base na resposta:
+// 1 -> “Bom dia”
+// 2 -> “Boa tarde”
+// 3 -> “Boa noite”
+
+// 6. Ler um valor N. Calcular e escrever seu respectivo fatorial. Fatorial de N = N * (N-1) * (N-2) * (N-3) * ... * 1.
+// Lembrando que, por definição, fatorial de 0 é 1.
+
+
+
+// 7. Escreva um programa para ler um número inteiro positivo N. O programa deve então mostrar na tela N linhas,
+// começando de 1 até N. Para cada linha, mostrar o número da linha, depois o quadrado e o cubo do valor. Ex.:
+// Entrada: 2
+// Saída:
+// 1 1 1
+// 2 4 8
+
+// 8. Escreva um programa que simule a urna eletrônica. A tela a ser apresentada deverá ser da seguinte forma:
+// As opções são:
+// 1. Candidato Chaves
+// 2. Candidata Chiquinha
+// 3. Candidato Quico
+// 4. Nulo/Branco
+// 5. Encerrar a votação
+// Entre com o seu voto:
+// O programa deverá ler os votos dos eleitores e, quando for entrado o número 5, apresentar as seguintes
+// informações:
+// a) O número de votos de cada candidato;
+// b) A porcentagem de votos nulos;
+// c) A porcentagem de votos brancos;
+// d) O candidato vencedor
